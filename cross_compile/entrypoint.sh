95d8963ee080578e69bf5335ffbc51d056815627
joshuaboud <joshuaboud@hotmail.com>
2021-08-09 13:06:29 -0300

    commit updates

--- cross_compile/entrypoint.sh
+++ cross_compile/entrypoint.sh
@@ -13,8 +13,10 @@ mv defconfig /config_out
 make -j$(nproc) ARCH=$ARCH_ CROSS_COMPILE=$CROSS_COMPILE_ $IMAGE_ modules dtbs
 [[ "$?" != "0" ]] && exit 1
 
-env PATH=\$PATH make ARCH=$ARCH_ CROSS_COMPILE=$CROSS_COMPILE_ INSTALL_MOD_PATH=/rootfs_out modules_install
+env PATH=$PATH make ARCH=$ARCH_ CROSS_COMPILE=$CROSS_COMPILE_ INSTALL_MOD_PATH=/rootfs_out modules_install
 [[ "$?" != "0" ]] && exit 1
+
+mkdir -p /boot_out/overlays
 
 cp arch/$ARCH_/boot/$IMAGE_ /boot_out/$KERNEL.img
 cp arch/$ARCH_/boot/dts/broadcom/*.dtb /boot_out/
